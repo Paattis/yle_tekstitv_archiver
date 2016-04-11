9485e06a8d3f3562600f8ee3ebf533c23b60392f
Leevi Paattilammi <leevi.paattilammi@gmail.com>
2016-04-11 15:03:04 +0300

    -improved output -improved python 2.7 compatibility

--- tekstitv_archiver.py
+++ tekstitv_archiver.py
@@ -10,9 +10,10 @@ firstPage = 100
 lastPage = 899
 maxSubPages = 30
 
+sleepTime = 5
+
 subPageNums = list(map(lambda x: x if int(x) < 9 else x[1:],["0"+str(i) for i in range(1,maxSubPages+1)]))
 
-print(subPageNums)
 date = datetime.datetime.today().strftime("%d_%m_%Y")
 basePath ="./archived_pages/%s" % date
 
@@ -25,10 +26,12 @@ def getPicture(page,savePath,subPage="01",):
 		print(pictureUrl)
 		r = requests.get(pictureUrl,headers=headers);
 	except:
+		print("save failed")
 		return False
 
 	#page or subpage doesn't exist
 	if r.status_code != 200:
+		print("save failed")
 		return False
 
 	
@@ -46,15 +49,28 @@ def getPicture(page,savePath,subPage="01",):
 		pictureSavePath = os.path.realpath(pictureSavePath)
 		urllib.request.urlretrieve(pictureUrl,pictureSavePath)
 		return True
-	except:
-		return False
+	except AttributeError:
+		#python 2.7
+		try:
+			pictureSavePath = basePath+"/"+pictureUrl.split("/")[-1]
+			pictureSavePath = os.path.realpath(pictureSavePath)
+			urllib.urlretrieve(pictureUrl,pictureSavePath)
+			return True
+		except:
+			return False
+		
 
+savedPageCount = 0
 for pageNum in range(firstPage,lastPage+1):
 	for subPageNum in subPageNums:
-		print(subPageNum)
+		print("Trying to save page %i subpage %s" % (pageNum,subPageNum))
+
 		if getPicture(pageNum,basePath,subPageNum):
 			print("Page %i subpage %s successfully saved" % (pageNum,subPageNum))
-		time.sleep(random.randint(1,5))
+			savedPageCount += 1
+			print("On page %i_%s out of %i possible pages, %i pages saved" % (pageNum,subPageNum,lastPage*maxSubPages,savedPageCount))
+
+		time.sleep(sleepTime)
 
 
 
